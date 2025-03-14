70daa5b6e8aa7adaf702bfc788d2094006acd367
sergerad <serge.radinovich@immutable.com>
2025-03-15 10:00:14 +1300

    Interactive mode for txlist

--- src/components/txlist.rs
+++ src/components/txlist.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 use color_eyre::Result;
 use ratatui::{prelude::*, widgets::*};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
@@ -5,6 +7,7 @@ use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use super::Component;
 use crate::{
     action::Action,
+    app::Mode,
     config::Config,
     types::{Abridged, Transaction},
 };
@@ -14,8 +17,10 @@ pub struct TxList {
     command_tx: Option<UnboundedSender<Action>>,
     config: Config,
     transaction_rx: Option<UnboundedReceiver<Vec<Transaction>>>,
-    transactions: Vec<Transaction>,
+    transactions: VecDeque<Vec<Transaction>>,
+    transactions_idx: usize,
     value_column_name: String,
+    mode: Mode,
 }
 
 impl TxList {
@@ -40,12 +45,33 @@ impl Component for TxList {
 
     fn update(&mut self, action: Action) -> Result<Option<Action>> {
         match action {
+            Action::Down => {
+                self.mode = Mode::Interactive;
+                self.transactions_idx = self
+                    .transactions_idx
+                    .saturating_add(1)
+                    .min(self.transactions.len().saturating_sub(1));
+            }
+            Action::Up => {
+                self.mode = Mode::Interactive;
+                self.transactions_idx = self.transactions_idx.saturating_sub(1);
+            }
+            Action::Follow => {
+                self.mode = Mode::Follow;
+                self.transactions_idx = 0usize;
+            }
             Action::Tick => {
-                if let Ok(transactions) = self.transaction_rx.as_mut().unwrap().try_recv() {
-                    if !transactions.is_empty() {
-                        self.transactions = transactions;
-                        if self.value_column_name.is_empty() {
-                            self.value_column_name = self.transactions[0].unit.to_uppercase();
+                if matches!(self.mode, Mode::Follow) {
+                    if let Ok(transactions) = self.transaction_rx.as_mut().unwrap().try_recv() {
+                        if !transactions.is_empty() {
+                            self.transactions.push_front(transactions);
+                            if self.transactions.len() > 20 {
+                                self.transactions.pop_back();
+                            }
+                            if self.value_column_name.is_empty() {
+                                self.value_column_name =
+                                    self.transactions[0][0].unit.to_uppercase();
+                            }
                         }
                     }
                 }
@@ -57,42 +83,42 @@ impl Component for TxList {
     }
 
     fn draw(&mut self, frame: &mut Frame, area: Rect) -> Result<()> {
-        let rows: Vec<_> = self
-            .transactions
-            .iter()
-            .map(|tx| {
-                Row::new(vec![
-                    tx.hash.clone(),
-                    tx.from.abridged(),
-                    tx.to.abridged(),
-                    tx.value.clone(),
-                ])
-            })
-            .collect();
-        let widths = [
-            Constraint::Percentage(40),
-            Constraint::Percentage(20),
-            Constraint::Percentage(20),
-            Constraint::Percentage(10),
-        ];
-        let table = Table::new(rows, widths)
-            .column_spacing(2)
-            .style(Style::new().blue())
-            .header(
-                Row::new(vec!["HASH", "FROM", "TO", &self.value_column_name])
-                    .style(Style::new().bold().italic()), // To add space between the header and the rest of the rows, specify the margin
-                                                          //.bottom_margin(1),
-            )
-            .block(
-                ratatui::widgets::Block::bordered()
-                    .title_bottom("TRANSACTIONS")
-                    .title_alignment(Alignment::Center),
-            )
-            .row_highlight_style(Style::new().reversed())
-            .column_highlight_style(Style::new().red())
-            .cell_highlight_style(Style::new().blue())
-            .highlight_symbol(">>");
-        frame.render_widget(table, area);
+        if let Some(transactions) = self.transactions.get(self.transactions_idx) {
+            let rows: Vec<_> = transactions
+                .iter()
+                .map(|tx| {
+                    Row::new(vec![
+                        tx.hash.clone(),
+                        tx.from.abridged(),
+                        tx.to.abridged(),
+                        tx.value.clone(),
+                    ])
+                })
+                .collect();
+            let widths = [
+                Constraint::Percentage(40),
+                Constraint::Percentage(20),
+                Constraint::Percentage(20),
+                Constraint::Percentage(10),
+            ];
+            let table = Table::new(rows, widths)
+                .column_spacing(2)
+                .style(Style::new().blue())
+                .header(
+                    Row::new(vec!["HASH", "FROM", "TO", &self.value_column_name])
+                        .style(Style::new().bold().italic()),
+                )
+                .block(
+                    ratatui::widgets::Block::bordered()
+                        .title_bottom("TRANSACTIONS")
+                        .title_alignment(Alignment::Center),
+                )
+                .row_highlight_style(Style::new().reversed())
+                .column_highlight_style(Style::new().red())
+                .cell_highlight_style(Style::new().blue())
+                .highlight_symbol(">>");
+            frame.render_widget(table, area);
+        }
         Ok(())
     }
 }
