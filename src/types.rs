2e2b4e2cbccda7d926f72e9b9563a189235eb839
sergerad <serge.radinovich@immutable.com>
2025-03-17 09:18:55 +1300

    Add comments and tests to types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,23 +1,6 @@
 use std::fmt::Debug;
 
-pub trait Abridged {
-    fn abridged(&self) -> String;
-}
-
-impl Abridged for String {
-    fn abridged(&self) -> String {
-        let prefix_len = 6;
-        let suffix_len = 4;
-        if self.len() <= prefix_len + suffix_len {
-            self.clone()
-        } else {
-            let prefix = &self[..prefix_len];
-            let suffix = &self[self.len() - suffix_len..];
-            format!("{}..{}", prefix, suffix)
-        }
-    }
-}
-
+/// Contains the chain-agnostic data required to represent a block in the UI.
 #[derive(Debug, Clone, Default)]
 pub struct Block {
     pub number: u64,
@@ -25,6 +8,7 @@ pub struct Block {
     pub timestamp: u64,
 }
 
+/// Contains the chain-agnostic data required to represent a transaction block in the UI.
 #[derive(Debug, Clone, Default)]
 pub struct Transaction {
     pub hash: String,
@@ -34,9 +18,47 @@ pub struct Transaction {
     pub unit: String,
 }
 
+/// Contains the chain-agnostic data required to represent an account block in the UI.
 #[derive(Debug, Clone, Default)]
 pub struct Account {
     pub address: String,
     pub balance: String,
     pub unit: String,
+}
+
+/// Produces a shortened `String` representation of some type.
+///
+/// Used as an extension for `String`.
+pub trait Abridged {
+    fn abridged(&self) -> String;
+}
+
+impl<T: AsRef<str>> Abridged for T {
+    /// Shortens the content by replacng content with ellipses.
+    fn abridged(&self) -> String {
+        let prefix_len = 6;
+        let suffix_len = 4;
+        let s = self.as_ref();
+        if s.len() <= prefix_len + suffix_len {
+            s.to_string()
+        } else {
+            let prefix = &s[..prefix_len];
+            let suffix = &s[s.len() - suffix_len..];
+            format!("{}..{}", prefix, suffix)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Abridged;
+    #[test]
+    fn abridged() {
+        // &str
+        let s = "012345abcd3210";
+        assert_eq!("012345..3210".to_string(), s.abridged());
+        // String
+        let s = "012345abcd3210".to_string();
+        assert_eq!("012345..3210".to_string(), s.abridged());
+    }
 }
