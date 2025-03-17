f8d36e8b616615e70f1f2144e1dc35a4d787a2e7
sergerad <serge.radinovich@immutable.com>
2025-03-17 18:39:55 +1300

    Add comments to monitor.rs and providers.rs

--- src/monitor.rs
+++ src/monitor.rs
@@ -1,12 +1,12 @@
-use std::time::Duration;
-
 use crate::providers::ChainProvider;
 use crate::types::{
     AccountReceiver, AccountSender, BlockReceiver, BlockSender, TransactionReceiver,
     TransactionSender,
 };
+use std::time::Duration;
 use tokio::{sync::mpsc::unbounded_channel, time::interval};
 
+/// Runtime responsible for managing retrieval of latest chain data.
 pub struct ChainMonitor<P> {
     block_tx: BlockSender,
     transaction_tx: TransactionSender,
@@ -18,6 +18,7 @@ pub struct ChainMonitor<P> {
 }
 
 impl<P> ChainMonitor<P> {
+    /// Constructs a new `ChainMonitor` based on a specific `ChainProvider`.
     pub fn new(provider: P) -> Self {
         let (block_tx, block_rx) = unbounded_channel();
         let (transaction_tx, transaction_rx) = unbounded_channel();
@@ -33,11 +34,12 @@ impl<P> ChainMonitor<P> {
         }
     }
 
-    /// ...
+    /// Relinquishes ownership of receivers for various chain data receivers
+    /// required for the delivery of chain data to the UI of the app.
     ///
     /// # Panics
     ///
-    /// ...
+    /// If this function is called more than once, it will panic.
     pub fn receivers(&mut self) -> (BlockReceiver, TransactionReceiver, AccountReceiver) {
         (
             self.block_rx.take().unwrap(),
@@ -48,17 +50,24 @@ impl<P> ChainMonitor<P> {
 }
 
 impl<P: ChainProvider + Sync> ChainMonitor<P> {
+    /// The main loop of the `ChainMonitor`.
     pub async fn run(&mut self, tick_rate: Duration) {
         let mut tick_interval = interval(tick_rate);
         let mut head_number = 0u64;
         loop {
             tick_interval.tick().await;
+
+            // Retrieve the latest block.
             let block = self.provider.head().await.unwrap();
+            // Do not send duplicate blocks.
             if block.number > head_number {
                 head_number = block.number;
+                // Send the block.
                 self.block_tx.send(vec![block]).unwrap();
+                // Get and send the transactions.
                 let txs = self.provider.transactions().await.unwrap();
                 self.transaction_tx.send(txs).unwrap();
+                // Get and send the account balances.
                 let bals = self.provider.balances().await.unwrap();
                 self.account_tx.send(bals).unwrap();
             }
--- src/providers.rs
+++ src/providers.rs
@@ -2,10 +2,25 @@ pub mod eth;
 
 use crate::types::{Account, Block, Transaction};
 
+/// Represents a service that provides chain data in the form of blocks, transactions, and accounts.
+///
+/// The data provided is designed to be chain agnostic. It is used to render
+/// information to the UI of the app.
 #[async_trait::async_trait]
 pub trait ChainProvider {
     type Error: std::fmt::Debug;
+
+    /// Retrieve the latest head of the blockchain.
+    ///
+    /// Should be used to cache any data required to serve data derived from
+    /// specific blocks such as transaction data.
     async fn head(&mut self) -> Result<Block, Self::Error>;
+
+    /// Retrieve the transactions pertaining to the last block retrieved
+    /// from the chain.
     async fn transactions(&self) -> Result<Vec<Transaction>, Self::Error>;
+
+    /// Retrieve the account balances pertaining to the last block retrieved
+    /// from the chain.
     async fn balances(&self) -> Result<Vec<Account>, Self::Error>;
 }
